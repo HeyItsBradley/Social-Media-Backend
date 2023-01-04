af1a4657c4e01c3f6c3f55536cfb7f87a0183036
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-04 15:49:26 -0700

    completed the users schema model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const assignmentSchema = require("./Assignment");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "thought",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+});
+
+userSchema.virtual("friendCount").get(() => {
+  return this.friends.length;
+});
+
+const User = mongoose.model("user", userSchema);
