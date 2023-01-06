369ed49b7a1c13df64f6221daf016c12f617fbbb
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-05 20:55:02 -0700

    added the rest of the crud ops for the userContoller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,4 +54,36 @@ module.export = {
         res.status(500).json(err);
       });
   },
+  addThought(req, res) {
+    console.log("you are adding a thought");
+    console.log(req.body);
+    user
+      .findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { thoughts: req.body } },
+        { runValidators: true, new: true }
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that Id" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+  removeThought(req, res) {
+    user
+      .findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { thought: { thoughtId: req.params.thoughtId } } },
+        { runValidators: true, new: true }
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "no user found with that id" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
